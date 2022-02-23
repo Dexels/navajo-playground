550e8a9818801d16b84b69a868bae14fad40a220
mbergman <mbergman@dexels.com>
2022-02-23 15:22:06 +0100

    Added two files.

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class HelloWorld {
+
+  public HelloWorld() {
+  }
+
+  public static void main( String[] args ) {
+    HelloWorld myTest = new HelloWorld();
+
+    System.out.println( "Hello world (what else?)" );
+  }
+}
